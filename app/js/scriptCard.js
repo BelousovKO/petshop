f1c725fbddf7f1990d698ac917499ef2973f238c
belousovko <working.belousovko@gmail.com>
2021-08-09 21:27:57 +0700

    реализована страницы карты товара

--- app/js/scriptCard.js
+++ app/js/scriptCard.js
@@ -7,141 +7,110 @@ const DiseasesRequest = new XMLHttpRequest();
 let diagnosesList
 
 DiseasesRequest.onload = function (e) {
-  diagnosesList = JSON.parse(DiseasesRequest.responseText);
+  init();
 };
 
 DiseasesRequest.open("GET", "http://localhost:3000/api/directoryDiseases.json", true);
 DiseasesRequest.send();
 
-const xhr = new XMLHttpRequest();
-
-xhr.onload = function (e) {
-  init();
-};
-
-xhr.open("GET", "http://localhost:3000/api/dataCards.json", true);
-xhr.send();
 
 function init() {
 
-  const infoCards = JSON.parse(xhr.responseText);
-  const id = new URL(window.location.href).searchParams.get('id');
-  document.querySelector('.img-pet').src = infoCards[id].img;
-  document.querySelector('#typePet').textContent = addTypesPet();
-  document.querySelector('#sizePet').textContent = AddSizePet();
 
-  function addTypesPet() {
-    let res = '';
-    infoCards[id].hunter ? res = res.concat('охотничья, ') : '';
-    infoCards[id].companion ? res = res.concat('компаньён, ') : '';
-    infoCards[id].decorative ? res = res.concat('декоративная, ') : '';
-    infoCards[id].service ? res = res.concat('служебная, ') : '';
-    res = res.slice(0, 1).toUpperCase() + res.slice(1, res.length - 2);
-    return res;
-  }
+  const xhr = new XMLHttpRequest();
 
-  function AddSizePet() {
-    let res = '';
-    infoCards[id].size = 1 ? res = 'Маленький' : '';
-    infoCards[id].size = 2 ? res = 'Средний' : '';
-    infoCards[id].size = 3 ? res = 'Большой' : '';
-    infoCards[id].size = 4 ? res = 'Маленький' : '';
-    return res;
+  xhr.onload = function (e) {
+    init1();
+  };
+
+  xhr.open("GET", "http://localhost:3000/api/dataCards.json", true);
+  xhr.send();
+
+  function init1() {
+
+    diagnosesList = JSON.parse(DiseasesRequest.responseText);
+
+    const infoCards = JSON.parse(xhr.responseText);
+    const id = new URL(window.location.href).searchParams.get('id');
+    const buttonMobileMenu = document.querySelector('.inCatalog-mobile');
+    const exitMobileMenu = document.querySelector('.out-mobile-menu');
+    const buttonMobileBox = document.querySelector('.button-in-box');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const sectionBox = document.querySelector('.section-box');
+    const shadowBox = document.querySelector('.shadow-box');
+
+    document.querySelector('h2').textContent = infoCards[id].breed;
+    document.querySelector('.img-pet').src = infoCards[id].img;
+    document.querySelector('#typePet').textContent = addTypesPet();
+    document.querySelector('#sizePet').textContent = addSizePet();
+    document.querySelector('#subtypes').textContent = addSubtypes();
+    document.querySelector('#illness').textContent = addDiagnoses();
+    document.querySelector('#intelligence').textContent = infoCards[id].intelligence;
+    document.querySelector('.price').textContent = `${infoCards[id].price} ₽`;
+    document.querySelector('.inBox').dataset.idcards = id;
+
+    box.addListenerModalBasket();
+    box.addListenerButtonsInBox(infoCards)
+    box.calculatingBoxCounter(infoCards);
+    box.addListenerDelAllItemInBox();
+
+    function addTypesPet() {
+      let res = '';
+      infoCards[id].hunter ? res = res.concat('охотничьи, ') : '';
+      infoCards[id].companion ? res = res.concat('компаньёны, ') : '';
+      infoCards[id].decorative ? res = res.concat('декоративные, ') : '';
+      infoCards[id].service ? res = res.concat('служебные, ') : '';
+      res = res.slice(0, 1).toUpperCase() + res.slice(1, res.length - 2);
+      return res;
+    }
+
+    function addSizePet() {
+      let res = '';
+      infoCards[id].size === 1 ? res = 'Маленькая (до 11кг)' : '';
+      infoCards[id].size === 2 ? res = 'Средняя (11-25кг)' : '';
+      infoCards[id].size === 3 ? res = 'Большая (25-50кг)' : '';
+      infoCards[id].size === 4 ? res = 'Огромная (больше 50кг)' : '';
+      return res;
+    }
+
+    function addSubtypes() {
+      let res = '';
+      infoCards[id].noFear ? res = res.concat('отсутствует чувство страха, ') : '';
+      infoCards[id].barksALittle ? res = res.concat('мало лают, ') : '';
+      infoCards[id].excellentHealth ? res = res.concat('отличное здоровье, ') : '';
+      infoCards[id].goodObedience ? res = res.concat('хорошее послушание, ') : '';
+      infoCards[id].veryDevoted ? res = res.concat('очень преданные, ') : '';
+      res = res.slice(0, 1).toUpperCase() + res.slice(1, res.length - 2);
+      return res;
+    }
+
+    function addDiagnoses() {
+      let res = '';
+      infoCards[id].frequentIllnesses.forEach(Illness => {
+        res = res.concat(`${diagnosesList[Illness]}, `)
+      });
+      res = res.slice(0, 1).toUpperCase() + res.slice(1, res.length - 2);
+      return res;
+    }
+
+    buttonMobileMenu.addEventListener('click', () => {
+      mobileMenu.style.display = 'block';
+      buttonMobileMenu.style.display = 'none';
+      exitMobileMenu.style.display = 'block';
+    });
+
+    exitMobileMenu.addEventListener('click', () => {
+      exitMobileMenu.style.display = 'none';
+      buttonMobileMenu.style.display = 'block';
+      mobileMenu.style.display = 'none';
+    });
+
+    buttonMobileBox.addEventListener('click', () => {
+      exitMobileMenu.style.display = 'none';
+      buttonMobileMenu.style.display = 'block';
+      mobileMenu.style.display = 'none';
+      sectionBox.style.display = "block";
+      shadowBox.style.display = "block";
+    })
   }
 }
-
-
-//
-//   const infoCards = JSON.parse(xhr.responseText);
-//   const id = new URL(window.location.href).searchParams.get('id');
-//   const parentItem = document.querySelector('.main');
-//   const typePet = parentItem.querySelector('#typePet');
-//   const sizePet = parentItem.querySelector('#sizePet');
-//   const subtypes = parentItem.querySelector('#subtypes');
-//   const intelligence = parentItem.querySelector('#intelligence');
-//   const price = document.querySelector('.price');
-//   const illness = parentItem.querySelector('#illness');
-//   const inMobileMenu = document.querySelector('.inCatalog-mobile');
-//   const mobileMenu = document.querySelector('.mobile-menu');
-//   const closeMobileMenu = document.querySelector('.out-mobile-menu');
-//   const buttonInBox = document.querySelector('.button-in-box');
-//   const boxModal = document.querySelector('.section-box');
-//
-//   infoCards.forEach(itemCard => {
-//     if (Number(id) === Number(itemCard.id)) {
-//       parentItem.querySelector('h1').textContent = itemCard.breed;
-//       parentItem.querySelector('img').src = `../${itemCard.img}`;
-//       parentItem.querySelector('h2').textContent = `Характеристика ${itemCard.breed}`;
-//       parentItem.querySelector('.inBox').dataset.idcards = id;
-//
-//       if (itemCard.hunter) {
-//         typePet.textContent = `Охотничьи`;
-//       } else {
-//         typePet.textContent = ''
-//       }
-//       if (itemCard.companion) {
-//         typePet.textContent = `${typePet.textContent} Компаньоны`
-//       }
-//       if (itemCard.decorative) {
-//         typePet.textContent = `${typePet.textContent} Декоративные`
-//       }
-//       if (itemCard.service) {
-//         typePet.textContent = `${typePet.textContent} Служебные`
-//       }
-//
-//       if (itemCard.size === 1) {
-//         sizePet.textContent = `Маленькая (меньше 11кг)`
-//       }
-//       if (itemCard.size === 2) {
-//         sizePet.textContent = 'Средняя (11-25кг)';
-//       }
-//       if (itemCard.size === 3) {
-//         sizePet.textContent = 'Большая (25-100кг)';
-//       }
-//       if (itemCard.size === 4) {
-//         sizePet.textContent = "Огромная (больше 100кг)";
-//       }
-//
-//       if (itemCard.noFear) {
-//         subtypes.textContent = `Отсутствует чуство страха`
-//       }
-//       if (itemCard.shedsLittle) {
-//         subtypes.textContent = `${subtypes.textContent} Мало линяет`
-//       }
-//       if (itemCard.goodObedience) {
-//         subtypes.textContent = `${subtypes.textContent} Хорошее послушание`
-//       }
-//       if (itemCard.veryDevoted) {
-//         subtypes.textContent = `${subtypes.textContent} Очень преданная`
-//       }
-//
-//       intelligence.textContent = itemCard.intelligence;
-//
-//       price.textContent = itemCard.price;
-//
-//       illness.textContent = '';
-//       for (let i = 0; i < infoCards[id].frequentIllnesses.length; i++) {
-//         illness.textContent = illness.textContent + " " + diseases[infoCards[id].frequentIllnesses[i]];
-//       }
-//     }
-//   });
-//
-//   inMobileMenu.addEventListener('click', () => {
-//     mobileMenu.style.display = 'block';
-//     inMobileMenu.style.display = 'none';
-//     closeMobileMenu.style.display = 'block';
-//   });
-//
-//   closeMobileMenu.addEventListener('click', () => {
-//     mobileMenu.style.display = 'none';
-//     inMobileMenu.style.display = 'block';
-//     closeMobileMenu.style.display = 'none';
-//   })
-//
-//   buttonInBox.addEventListener('click', () => {
-//     boxModal.style.display = 'block';
-//     mobileMenu.style.display = 'none';
-//     inMobileMenu.style.display = 'block';
-//     closeMobileMenu.style.display = 'none';
-//   })
-// }
